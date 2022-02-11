9089715782def382acd7d9547d005ca99453c9d7
Andrew Hayman <andrewrhayman@gmail.com>
2022-02-10 19:16:57 -0500

    Added timing tests to run on CAC

--- tf_quantum_layers.py
+++ tf_quantum_layers.py
@@ -30,8 +30,8 @@ def real_amplitudes_circuit(n_qubits, n_blocks):
     circuit = qml.qnn.KerasLayer(real_amplitudes, weight_shapes, output_dim=n_qubits)
     return circuit
 
-def cv_neural_net(n_qubits, n_blocks):
-    dev = qml.device("strawberryfields.tf", wires=n_qubits, cutoff_dim=15)
+def cv_neural_net(n_qubits, n_blocks, cutoff_dim):
+    dev = qml.device("strawberryfields.tf", wires=n_qubits, cutoff_dim=cutoff_dim)
 
     @qml.qnode(dev, interface="tf")
     def cv_nn(inputs, w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10):
@@ -45,6 +45,10 @@ def cv_neural_net(n_qubits, n_blocks):
     weight_shapes = {"w0":(L,K), "w1":(L,K), "w2":(L,M), "w3":(L,M), "w4":(L,M), "w5":(L,K), "w6":(L,K), "w7":(L,M), "w8":(L,M), "w9":(L,M), "w10":(L,M)}
     circuit = qml.qnn.KerasLayer(cv_nn, weight_shapes, output_dim=2)
     return circuit
+
+def basic_entangling_layers(n_qubits, n_blocks):
+    dev = qml.device("default.qubit.tf", wires=n_qubits)
+
 
 class QuantumLayer(keras.Model):
     def __init__(self, input_size, circuit_layer, **kwargs):
@@ -76,7 +80,7 @@ class QuantumLayer(keras.Model):
                 raise ValueError("Please ensure the input size is a multiple of the number of wires")
             self.num_circuits = int(input_size / self.num_wires)
 
-            self.circuit_layer = [cv_neural_net(n_qubits, n_blocks) for i in range(self.num_circuits)]
+            self.circuit_layer = [cv_neural_net(n_qubits, n_blocks, kwargs['cutoff_dim']) for i in range(self.num_circuits)]
 
     def call(self, x):
         x_split = list(tf.split(x, self.num_circuits, axis=1))
