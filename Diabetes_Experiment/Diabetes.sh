bf120e64dea6745f039821fdca35db5db39d30f1
Andrew Hayman <andrewrhayman@gmail.com>
2022-03-15 12:56:58 -0400

    Added new slurm script for next experiment

--- Diabetes_Experiment/Diabetes.sh
+++ Diabetes_Experiment/Diabetes.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-#SBATCH --array=0-2
+#SBATCH --array=0-26
 #SBATCH --job-name=test_sweep
 #SBATCH --output=output.txt
 #SBATCH --mem=32GB
@@ -10,10 +10,13 @@
 #SBATCH --mail-user=18arth@queensu.ca
 
 encoding_strategy_values=( None Sigmoid Sigmoid_BatchNorm )
-cutoff_dimension_values=( 15 )
+cutoff_dimension_values=( 5 10 15 )
+num_layers_values=( 1 2 3 )
 trial=${SLURM_ARRAY_TASK_ID}
 encoding_strategy=${encoding_strategy_values[$(( trial % ${#encoding_strategy_values[@]} ))]}
 trial=$(( trial / ${#encoding_strategy_values[@]} ))
 cutoff_dimension=${cutoff_dimension_values[$(( trial % ${#cutoff_dimension_values[@]} ))]}
+trial=$(( trial / ${#cutoff_dimension_values[@]} ))
+num_layers=${num_layers_values[$(( trial % ${#num_layers_values[@]} ))]}
 
-python Diabetes_Experiment.py with encoding_strategy=${encoding_strategy} cutoff_dimension=${cutoff_dimension} 
+python Diabetes_Experiment.py with encoding_strategy=${encoding_strategy} cutoff_dimension=${cutoff_dimension} num_layers=${num_layers} 
