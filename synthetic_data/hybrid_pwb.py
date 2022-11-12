513385c4839bcd30acc889b2b686036ba56b093f
Andrew Hayman <andrewrhayman@gmail.com>
2022-11-12 13:11:07 -0500

    Preparing for hybrid pwb experiments

--- synthetic_data/hybrid_pwb.py
+++ synthetic_data/hybrid_pwb.py
@@ -1,4 +1,9 @@
 """Hybrid Network Models 2022"""
+import copy
+import json
+from os import listdir
+from os.path import isdir, join
+
 import tensorflow as tf
 from data import generate_synthetic_dataset
 from hybrid_base import EXPERIMENT_NAME as BASE_EXPERIMENT_NAME
@@ -10,11 +15,27 @@ from sacred.utils import apply_backspaces_and_linefeeds
 
 from common_packages.CV_quantum_layers import Activation_Layer, CV_Measurement, QuantumLayer_MultiQunode
 from common_packages.PWBLayer_TF import PWBLinearLayer
+from common_packages.utilities import get_num_parameters_per_layer
 
 EXPERIMENT_NAME = "Synthetic_Hybrid_PWB_Experiment"
 ex = Experiment(EXPERIMENT_NAME)
 ex.observers.append(FileStorageObserver(EXPERIMENT_NAME))
 ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+
+def get_config(experiment_path):
+    """Utility: Get the config for an experiment"""
+    config_path = experiment_path + "/config.json"
+    with open(file=config_path, mode="r", encoding="utf-8") as json_file:
+        config = json.load(json_file)
+
+    config_copy = copy.deepcopy(config)
+    for key, val in config_copy.items():
+        if val is None:
+            config[key] = "None"
+        if key == "seed":
+            del config[key]
+    return config
 
 
 @ex.capture
@@ -29,12 +50,12 @@ def log_performance(_run, val_accuracy, val_loss, epoch):
 def confnet_config():
     """Default config"""
     precision = 8  # pylint: disable=W0612
-    exp_num = 1  # pylint: disable=W0612
-    network_type = "classical"  # pylint: disable=W0612
+    num_qumodes = 5  # pylint: disable=W0612
+    network_type = "quantum"  # pylint: disable=W0612
 
 
 @ex.automain
-def define_and_train(precision, exp_num, network_type):
+def define_and_train(precision, num_qumodes, network_type):
     """Build and run the network"""
 
     tf.random.set_seed(RANDOM_SEED)
@@ -48,7 +69,7 @@ def define_and_train(precision, exp_num, network_type):
             self.base_model = models.Sequential(
                 [
                     PWBLinearLayer(10, activation="relu", precision=precision),
-                    PWBLinearLayer(6, activation=None, precision=precision),
+                    PWBLinearLayer(2 * num_qumodes, activation=None, precision=precision),
                 ]
             )
 
@@ -56,7 +77,7 @@ def define_and_train(precision, exp_num, network_type):
                 self.quantum_substitue = models.Sequential(
                     [
                         layers.Dense(
-                            33,
+                            get_num_parameters_per_layer(num_qumodes),
                             activation="softmax",
                             bias_constraint=lambda t: tf.clip_by_value(t, -1.0, 1.0),
                             kernel_constraint=lambda t: tf.clip_by_value(t, -1.0, 1.0),
@@ -65,10 +86,10 @@ def define_and_train(precision, exp_num, network_type):
                 )
 
             self.quantum_layer = QuantumLayer_MultiQunode(
-                n_qumodes=3,
+                n_qumodes=num_qumodes,
                 n_circuits=1,
                 n_layers=1,
-                cutoff_dim=5,
+                cutoff_dim=3,
                 encoding_method="Amplitude_Phase",
                 regularizer=regularizers.L1(l1=0.1),
                 max_initial_weight=None,
@@ -93,11 +114,18 @@ def define_and_train(precision, exp_num, network_type):
             output = self.final_layer(output)
             return output
 
-    _, _, validate_data = generate_synthetic_dataset()
+    _, validate_data = generate_synthetic_dataset()
     model = Net()
     model.compile(optimizer=OPTIMIZER, loss=LOSS_FUNCTION, metrics=["accuracy"])
-    exp_path = f"{BASE_EXPERIMENT_NAME}/{exp_num}"
+
+    sub_folders = [folder for folder in listdir(BASE_EXPERIMENT_NAME) if isdir(join(BASE_EXPERIMENT_NAME, folder))]
+    experiment_numbers = [sub_folder for sub_folder in sub_folders if sub_folder.isdigit()]
+    for experiment_num in experiment_numbers:
+        config = get_config(f"{BASE_EXPERIMENT_NAME}/{experiment_num}")
+        if config["num_qumodes"] == num_qumodes and config["network_type"] == network_type:
+            target_experiment_path = f"{BASE_EXPERIMENT_NAME}/{experiment_num}"
+            break
     for i in range(NUM_EPOCHS):
-        model.load_weights(f"{exp_path}/weights/weight{i}.ckpt")
+        model.load_weights(f"{target_experiment_path}/weights/weight{i}.ckpt")
         val_loss, val_acc = model.evaluate(*validate_data, verbose=3)
         log_performance(val_accuracy=val_acc, val_loss=val_loss, epoch=i)  # pylint: disable=E1120
