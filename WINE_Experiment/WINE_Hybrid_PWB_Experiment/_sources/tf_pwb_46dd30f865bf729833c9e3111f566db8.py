dd2322150d7099334d38dff37f2826246088f26f
Tristan <17tna@queensu.ca>
2023-02-17 13:00:23 -0500

    Add wine plotting and PWB experement

---
+++ WINE_Experiment/WINE_Hybrid_PWB_Experiment/_sources/tf_pwb_46dd30f865bf729833c9e3111f566db8.py
@@ -0,0 +1,173 @@
+"""Hybrid Network Models 2022"""
+import copy
+import json
+from os import listdir
+from os.path import isdir, join
+
+import tensorflow as tf
+from WINE_Dataset import *
+from hybrid_base import EXPERIMENT_NAME as _BASE_EXPERIMENT_NAME
+
+from hybrid_base import LOSS_FUNCTION, NUM_EPOCHS, OPTIMIZER, RANDOM_SEED
+from keras import Model, layers, models, regularizers
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+
+from common_packages.CV_quantum_layers import Activation_Layer, CV_Measurement, QuantumLayer_MultiQunode
+from PWBLayer_TF import PWBLinearLayer
+from common_packages.utilities import get_equivalent_classical_layer_size
+
+EXPERIMENT_NAME = "WINE_Hybrid_PWB_Experiment"
+ex = Experiment(EXPERIMENT_NAME)
+ex.observers.append(FileStorageObserver(EXPERIMENT_NAME))
+ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+
+def get_config(experiment_path):
+    """Utility: Get the config for an experiment"""
+    config_path = experiment_path + "/config.json"
+    with open(file=config_path, mode="r", encoding="utf-8") as json_file:
+        config = json.load(json_file)
+
+    config_copy = copy.deepcopy(config)
+    for key, val in config_copy.items():
+        if val is None:
+            config[key] = "None"
+        if key == "seed":
+            del config[key]
+    return config
+
+
+@ex.capture
+def log_performance(_run, val_accuracy, val_loss, epoch):
+    """Logs performance with sacred framework"""
+    _run.log_scalar("val_loss", float(val_loss))
+    _run.log_scalar("val_accuracy", float(val_accuracy))
+    _run.log_scalar("epoch", int(epoch), epoch)
+
+
+@ex.config
+def confnet_config():
+    """Default config"""
+    num_qumodes = 2  # pylint: disable=W0612
+    network_type = "classical_tf"  # pylint: disable=W0612
+    sigma = 0
+
+
+@ex.automain
+def define_and_train(num_qumodes, network_type, sigma):
+    """Build and run the network"""
+
+    tf.random.set_seed(RANDOM_SEED)
+
+    class Net(Model):  # pylint: disable=W0223
+        """Neural network model to train on"""
+
+        def __init__(self):
+            super().__init__()
+            precision = 2**15 - 1
+            self.gaussian = layers.GaussianNoise(sigma)
+            if network_type == 'classical':
+                self.base_model = models.Sequential(
+                    [
+                        PWBLinearLayer(
+                            40,
+                            input_dim=9,
+                            activation="relu",
+                            precision=precision),
+                    ]
+                )
+                
+                self.classical1 = models.Sequential([
+                        layers.Flatten(),
+                        PWBLinearLayer(
+                            2 * num_qumodes,
+                            activation=None,
+                            precision=precision
+                        )
+                ])
+                self.classical2 = PWBLinearLayer(
+                            2,
+                            activation="softmax",
+                            precision=precision
+                        )
+
+            if network_type == 'classical_tf':
+                self.base_model = models.Sequential(
+                    [
+                        layers.Dense(
+                            40,
+                            input_dim=9,
+                            activation="relu",
+                            ),
+                    ]
+                )
+                
+                self.classical1 = models.Sequential([
+                        layers.Flatten(),
+                        layers.Dense(
+                            2 * num_qumodes,
+                            activation=None
+                        )
+                ])
+                self.classical2 = PWBLinearLayer(
+                            2,
+                            activation="softmax"
+                        )
+
+            classical_size = get_equivalent_classical_layer_size(num_qumodes, 2*num_qumodes, 2) 
+            if network_type == "classical" or network_type=='classical_tf':
+                self.quantum_substitue = models.Sequential(
+                    [
+                        layers.Dense(
+                            classical_size,
+                            activation="relu"
+                        ),
+                    ]
+                )
+
+            self.quantum_layer = QuantumLayer_MultiQunode(
+                n_qumodes=num_qumodes,
+                n_circuits=1,
+                n_layers=1,
+                cutoff_dim=5,
+                encoding_method="Amplitude_Phase",
+                regularizer=regularizers.L1(l1=0.01),
+                max_initial_weight=0.2,
+                measurement_object=CV_Measurement("X_quadrature"),
+                shots=None,
+            )
+
+            self.quantum_preparation_layer = Activation_Layer("TanH", self.quantum_layer.encoding_object)
+        def call(self, inputs):  # pylint: disable=W0221
+            """Call the network"""
+            output = self.gaussian(inputs, training=True)
+            output = self.base_model(output)
+            output = self.classical1(output)
+            if network_type == "quantum_tf":
+                output = self.quantum_preparation_layer(output)
+                output = self.quantum_layer(output)
+            elif network_type == "classical_tf":
+                output = self.quantum_substitue(output)
+            else:
+                raise ValueError("Invalid network type specified.")
+            output = self.classical2(output)
+            return output
+
+    x_val,y_val = load_validation()
+    validate_data = [x_val.astype(np.float32),y_val.astype(np.float32)]
+    model = Net()
+    model.compile(optimizer=OPTIMIZER, loss=LOSS_FUNCTION, metrics=["accuracy"])
+    BASE_EXPERIMENT_NAME = _BASE_EXPERIMENT_NAME
+    sub_folders = [folder for folder in listdir(BASE_EXPERIMENT_NAME) if isdir(join(BASE_EXPERIMENT_NAME, folder))]
+    experiment_numbers = [sub_folder for sub_folder in sub_folders if sub_folder.isdigit()]
+    for experiment_num in experiment_numbers:
+        config = get_config(f"{BASE_EXPERIMENT_NAME}/{experiment_num}")
+        if config["num_qumodes"] == num_qumodes and config["network_type"] == network_type:
+            target_experiment_path = f"{BASE_EXPERIMENT_NAME}/{experiment_num}"
+            break
+    for i in range(10):
+        model.load_weights(f"{target_experiment_path}/weights/weight{NUM_EPOCHS-1}.ckpt", by_name=False)
+        val_loss, val_acc = model.evaluate(*validate_data, verbose=3)
+        log_performance(val_accuracy=val_acc, val_loss=val_loss, epoch=i)  # pylint: disable=E1120
