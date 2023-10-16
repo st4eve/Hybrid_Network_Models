3b02b6b43a9ac5e2de3c824d95f64d60ac70b171
Tristan <17tna@queensu.ca>
2023-10-15 23:51:11 -0400

    More efficient fisher information calculation

--- synthetic_data/CalculateFisherInformation.py
+++ synthetic_data/CalculateFisherInformation.py
@@ -17,6 +17,7 @@ from numba import jit, njit
 import scipy
 import pandas as pd
 from common_packages.CV_quantum_layers import QuantumLayer
+from tqdm import tqdm
 
 
 #%%
@@ -30,18 +31,17 @@ class Calculate_Fisher_Information_Matrix():
         """Helper class to calculate the fisher information matrix for a given model
 
         Args:
-            model_func (Keras Model Initialization Function): Model to be tested
+            model_func (Keras Model Initialization Function): Function to generate model to be tested
             x_train (arr): Input data for the provided model
             n_iter (int, optional): Number of random model initializations to calculate the fisher information over. Defaults to 10.
         """
         self.model_func = model_func
-        self.models = [self.model_func() for _ in range(n_iter)]
+        self.model = self.model_func()
         self.test_model = self.model_func()
         self.x_train = np.array(x_train)
         self.test_model(self.x_train[0:1])
         self.n_iter = n_iter
-        self.models = [model_func() for _ in range(n_iter)]
-        self.inputs = np.random.uniform(-1, 1, size=[n_iter] + list(self.x_train.shape))
+        self.inputs = np.random.uniform(0, 1, size=[n_iter] + list(self.x_train.shape))
         self.n_classes = self.test_model.layers[-1].output_shape[-1]
         
         self.num_params = self.calc_total_params(self.test_model.trainable_variables)
@@ -91,13 +91,26 @@ class Calculate_Fisher_Information_Matrix():
                     param_vol *= 1
         return param_vol
                 
+    def reinitialize_model(self, model):
+        for l in model.layers:
+            if 'quantum_layer_multi_qunode' in l.name:
+                l.initialize_circuit()
+            elif 'sequential' in l.name:
+                self.reinitialize_model(l)
+            else:
+                if hasattr(l,"kernel_initializer"):
+                    l.kernel.assign(l.kernel_initializer(tf.shape(l.kernel)))
+                if hasattr(l,"bias_initializer") and l.use_bias:
+                    l.bias.assign(l.bias_initializer(tf.shape(l.bias)))
+                if hasattr(l,"recurrent_initializer"):
+                    l.recurrent_kernel.assign(l.recurrent_initializer(tf.shape(l.recurrent_kernel)))
 
     def calc_fisher(self):
         grads = self.grads
         total_params = self.num_params
         n_classes = self.n_classes
         fishers = np.zeros([len(grads), total_params, total_params])
-        for i in range(len(grads)):
+        for i in tqdm(range(len(grads)), desc='Calculating Fishers'):
             grad = grads[i]
             temp_sum = np.zeros([n_classes, total_params, total_params])
             for j in range(n_classes):
@@ -114,24 +127,26 @@ class Calculate_Fisher_Information_Matrix():
         num_datapoints = len(self.x_train)
         
         fisher_trace = np.trace(np.mean(fishers, axis=0))
-        fishers = np.mean(np.reshape(fishers, [n_iter, num_datapoints, total_params, total_params]), axis=0)
+        fishers = np.average(np.reshape(fishers, [n_iter, num_datapoints, total_params, total_params]), axis=0)
         fhat = fishers * total_params * param_vol / fisher_trace
         return fhat, fisher_trace
 
 
     def calc_all_grads(self):
         total_params = self.num_params
-        inputs = self.inputs
-        models = self.models
+        inputs = tf.convert_to_tensor(self.inputs)
         n_classes = self.n_classes
-        grads = np.zeros([len(inputs), len(inputs[0]), n_classes, total_params]) 
-        for count,(i,m) in enumerate(zip(inputs, models)):
-            outputs = m(i)
-            grad = self.calc_jac(i, m)
+        grads = np.zeros([len(inputs), len(inputs[0]), n_classes, total_params])
+        for count,i in enumerate(tqdm(inputs, desc='Calculating Gradients')):
+            outputs = self.model(i)
+            grad = self.calc_jac(i, self.model)
+            outputs = outputs.numpy()
             grad = tf.concat([tf.reshape(g, [g.shape[0], g.shape[1], tf.reduce_prod(g.shape[2:])]) for g in grad], axis=-1).numpy()
+            grad[:,:,:] = grad[:,:,:] * np.sqrt(np.reshape(outputs, [*outputs.shape, 1]))
             grads[count] = grad
+            self.reinitialize_model(self.model)
         grads = grads.reshape([len(inputs) * len(inputs[0]), n_classes, total_params])
-        return grads
+        return tf.convert_to_tensor(grads)
     
     def calc_eigvals(self):
         fhat = self.fhat
