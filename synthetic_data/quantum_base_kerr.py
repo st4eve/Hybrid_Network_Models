5d5cfa04189ca8cf939b3ef93eff67b26a1a2ba8
Tristan <17tna@queensu.ca>
2024-06-22 16:54:33 -0400

    Push before working on dekstop

--- synthetic_data/quantum_base_kerr.py
+++ synthetic_data/quantum_base_kerr.py
@@ -9,9 +9,12 @@ from keras.utils.layer_utils import count_params
 from sacred import Experiment
 from sacred.observers import FileStorageObserver
 from sacred.utils import apply_backspaces_and_linefeeds
+import os
 
 from common_packages.CV_quantum_layers import Activation_Layer, CV_Measurement, QuantumLayer_MultiQunode
 from common_packages.utilities import get_equivalent_classical_layer_size
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 RANDOM_SEED = 30
 BATCH_SIZE = 32
@@ -20,7 +23,7 @@ NUM_EPOCHS = 200
 
 OPTIMIZER = Adam(learning_rate=0.001)
 LOSS_FUNCTION = "categorical_crossentropy"
-EXPERIMENT_NAME = "Classical_Small_kerr_2q_high_cutoff"
+EXPERIMENT_NAME = "Classical_Small_kerr_no_hidden"
 ex = Experiment(EXPERIMENT_NAME)
 ex.observers.append(FileStorageObserver(EXPERIMENT_NAME))
 ex.captured_out_filter = apply_backspaces_and_linefeeds
@@ -55,10 +58,11 @@ class LogPerformance(Callback):
 @ex.config
 def confnet_config():
     """Default config"""
-    network_type = "quantum"  # pylint: disable=W0612
-    num_qumodes = 2  # pylint: disable=W0612
-    cutoff=5
-    n_layers=1
+    network_type = "classical"  # pylint: disable=W0612
+    num_qumodes = 1  # pylint: disable=W0612
+    cutoff=-1
+    n_layers=0
+    input_nl = None
     iteration=-1
 
 class Net(Model):  # pylint: disable=W0223
@@ -69,6 +73,7 @@ class Net(Model):  # pylint: disable=W0223
             num_qumodes,
             cutoff,
             n_layers,
+            input_nl,
             max_initial_weight=None,):
         super().__init__(
         )
@@ -79,7 +84,7 @@ class Net(Model):  # pylint: disable=W0223
         self.n_layers = n_layers
         self.max_initial_weight = max_initial_weight
         if network_type=='classical':
-            input_activation = 'relu'
+            input_activation = input_nl
         else:
             input_activation = None
         self.input_layer = models.Sequential(
@@ -94,7 +99,7 @@ class Net(Model):  # pylint: disable=W0223
             ]
         )
 
-        if network_type == "classical":
+        if (network_type == "classical")  and (n_layers > 0):
             initial_layer_size = get_equivalent_classical_layer_size(num_qumodes, 5*num_qumodes, num_qumodes)
             self.quantum_substitue = [
                     layers.Dense(
@@ -113,7 +118,7 @@ class Net(Model):  # pylint: disable=W0223
                         kernel_constraint=lambda t: tf.clip_by_value(t, -1.0, 1.0),
                     )
                 ]
-            if n_layers > 2:
+            elif n_layers > 2:
                 layer_size1 = get_equivalent_classical_layer_size(num_qumodes, initial_layer_size, num_qumodes)
                 self.quantum_substitue += [
                     layers.Dense(
@@ -140,7 +145,7 @@ class Net(Model):  # pylint: disable=W0223
                         kernel_constraint=lambda t: tf.clip_by_value(t, -1.0, 1.0),
                     )
                 ]
-            
+        
             self.quantum_substitue = models.Sequential(self.quantum_substitue)
         if network_type=='quantum':
             self.quantum_layer = QuantumLayer_MultiQunode(
@@ -174,9 +179,9 @@ class Net(Model):  # pylint: disable=W0223
         if self.network_type == "quantum":
             output = self.quantum_preparation_layer(output)
             output = self.quantum_layer(output)
-        elif self.network_type == "classical":
+        elif (self.network_type == "classical") and (self.n_layers > 0):
             output = self.quantum_substitue(output)
-        else:
+        elif self.network_type != "classical" and self.network_type != "quantum":
             raise ValueError("Invalid network type specified.")
         output = self.final_layer(output)
         return output
@@ -184,15 +189,14 @@ class Net(Model):  # pylint: disable=W0223
 train_data, test_data = generate_synthetic_dataset_easy(num_datapoints=1000, n_features=8, n_classes=4)
 
 @ex.automain
-def define_and_train(network_type, num_qumodes, cutoff, n_layers):
+def define_and_train(network_type, num_qumodes, cutoff, n_layers, input_nl):
     """Build and run the network"""
-
-
     model = Net(
         network_type=network_type,
         num_qumodes=num_qumodes,
         cutoff=cutoff,
         n_layers=n_layers,
+        input_nl=input_nl
     )
     model.compile(optimizer=OPTIMIZER, loss=LOSS_FUNCTION, metrics=["accuracy"])
     model.fit(
